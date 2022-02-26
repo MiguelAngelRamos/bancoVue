55ca646eb25ff00f1f463222381c90883211e086
Miguel Angel Ramos <return.infox@gmail.com>
2022-02-25 21:04:41 -0300

    componente navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,16 +1,16 @@
 app.component('navbar', {
-  // servicios y titulo por medio de los props
-  template: `
+  props:['tituloprop', 'serviciosprop'],
+  template:`
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">{{ titulo }}</a>
+    <a class="navbar-brand" href="#">{{ tituloprop }}</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <!-- v-bind:key  ó :key -->
-        <li class="nav-item" v-for="(servicio, index) of servicios" v-bind:key="index">
+        <li class="nav-item" v-for="(servicio, index) of serviciosprop" v-bind:key="index">
           <a class="nav-link active" aria-current="page" href="#"> {{ servicio }}</a>
         </li>  
       </ul>
